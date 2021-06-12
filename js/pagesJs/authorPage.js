492b4d7718b69724bbbccda6b9abe3b9c1b2d450
ulugbek2997 <rustamovulugbek831@gmail.com>
2021-06-12 17:34:24 +0500

    finish authorPage UI

--- js/pagesJs/authorPage.js
+++ js/pagesJs/authorPage.js
@@ -1,13 +1,27 @@
-import { authorCard } from "../jsLib/createFunctions.js";
+import { baseURL, imgesUrl } from "../jsLib/articles.js";
+import { authorCard, createCard, render } from "../jsLib/createFunctions.js";
+import { getCardMain } from "../jsLib/getItemFunctions.js";
 
-fetch('http://192.144.37.95:8080/api/articles?langId=1')
+const url1 = new URL(window.location.href);
+let langId=url1.searchParams.get('langId');
+const url2 = new URL(window.location.href);
+let journalId=url2.searchParams.get('journalId');
+const url3 = new URL(window.location.href);
+let authorId=url2.searchParams.get('authorId');
+
+fetch(`${baseURL}langId=${langId}&journalId=${journalId}&authorId=${authorId}&size=5&offset=0`)
 .then((response) => {
   return response.json();
 })
 .then((data) => {
   console.log(data)
+  const  cardAuthor=authorCard(data,imgesUrl);
+  render(cardAuthor,"rowMain")
 
+  for(let index=0;index<data['length'];index++){
+    const  cardMain=getCardMain(data,index,imgesUrl);
+    const card=createCard(cardMain);
+    render(card,"rowMain")
+  }
   return data
 });
-
-authorCard
